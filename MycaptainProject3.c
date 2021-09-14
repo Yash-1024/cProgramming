8e76a3b8196e82f28bf43b5a45d1c3125953d6ef
Yash-1024 <89937433+Yash-1024@users.noreply.github.com>
2021-09-15 00:01:30 +0530

    Update MycaptainProject3.c
    slight changes as per tutor demand

--- MycaptainProject3.c
+++ MycaptainProject3.c
@@ -5,6 +5,10 @@ int main()
 int n;
 printf("Enter a number from 1-5\n");
 scanf("%d",&n);
+if(n!=1||n!=1||n!=2||n!=3||n!=4||n!=5)
+    printf("enter a number between 1-5");
+else
+{
 printf("Very well your food choice is :\n");
 printf("---------------------------------------------------\n");
 switch(n)
@@ -26,5 +30,6 @@ printf("| Food Choice : Veg Chowmin\t\t\t  |\n| Food Cost : 149\t\t\t\t  |");
 break;
 }
 printf("\n---------------------------------------------------\n");
+}
 return 0;
 }
